7550f5ed3debd701b281d1dee1fb7a4b7c7052a2
agnesdonat <agi.donat@gmail.com>
2018-03-03 15:30:16 +0000

    Dishes & prices have been added

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,7 +1,13 @@
 class Menu
 
   def show_menu
-    menu = []
+    menu = [
+      { "Garlic bread" => "£2.99" },
+      { "Olives" =>  "£3.99" },
+      { "Carbonara" => "£8.99" },
+      { "Four cheese pizza" => "£11.99" },
+      { "Tiramisu" => "£6.50" }
+    ]
   end
 
 end
--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -4,7 +4,12 @@ describe Menu do
 
   describe '#show_menu' do
     it 'shows available dishes and their prices' do
-      expect(subject.show_menu).to eq []
+      expect(subject.show_menu).to eq [{ "Garlic bread" => "£2.99" },
+      { "Olives" => "£3.99" },
+      { "Carbonara" => "£8.99" },
+      { "Four cheese pizza" => "£11.99" },
+      { "Tiramisu" => "£6.50" }]
     end
   end
+
 end
